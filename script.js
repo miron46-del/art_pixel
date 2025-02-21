6b703c56d19246bca94e0108a95a10a2a7ca2cfa
Kostya <t8055875@gmail.com>
2025-02-21 17:09:13 +0300

    добавление файлов

---
+++ script.js
@@ -0,0 +1,8 @@
+let button = document.querySelector('.button')
+button.addEventListener('click', function(){
+    location.assign('pixel.html')
+    
+})
+button.addEventListener('mouseenter', function(){
+    button.style.cursor = 'pointer'
+})
