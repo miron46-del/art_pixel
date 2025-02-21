acfe0426616a2dc0639332c1201bf8ac3d6a0868
Kostya <t8055875@gmail.com>
2025-02-21 17:08:31 +0300

    Delete Pixel directory

--- Pixel/script.js
+++
@@ -1,8 +0,0 @@
-let button = document.querySelector('.button')
-button.addEventListener('click', function(){
-    location.assign('pixel.html')
-    
-})
-button.addEventListener('mouseenter', function(){
-    button.style.cursor = 'pointer'
-})
